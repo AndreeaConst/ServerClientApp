24132eaf52ff1dd47b12847fe677388a3cd0a766
AndreeaConst <65601860+AndreeaConst@users.noreply.github.com>
2021-03-17 22:57:53 +0200

    Added server gRPC service

---
+++ PersonService/PeopleService/DataAccess/PersonOperations.cs
@@ -0,0 +1,67 @@
+﻿using PersonService;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PersonService.DataAccess
+{
+    public class PersonOperations
+    {
+        private readonly string filePath = "./Resources/people.txt";
+
+        public bool AddPerson(Person person)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(filePath, true);
+                sw.WriteLine(person.Name + "|" + person.Gender + "|" + person.Age + "|" + person.ID, true);
+                sw.Close();
+                Console.WriteLine("Adding client to list...");
+                Console.WriteLine("Name " + person.Name + ", gender " + person.Gender + ", age " + person.Age + ", ID " + person.ID);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+                return false;
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+
+            return true;
+        }
+
+        public void GetPeople()
+        {
+            
+            string line;
+            try
+            {
+                Console.WriteLine("Clients registered so far: ");
+
+                StreamReader sr = new StreamReader(filePath);
+                line = sr.ReadLine();
+                while (line != null)
+                {
+                    string[] broken_line = line.Split('|');
+
+                    Person person = new Person() { Name = broken_line[0], Gender = broken_line[1], Age = Convert.ToInt32(broken_line[2]), ID = broken_line[3] };
+
+                    Console.WriteLine("Name: " + person.Name + ", gender: " + person.Gender + ", age: " + person.Age + ", ID: " + person.ID);
+
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+    }
+}
---
+++ PersonService/PeopleService/Services/PersonService.cs
@@ -0,0 +1,40 @@
+﻿using PersonService.DataAccess;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonService.Services
+{
+    public class PersonService : PersonRegistration.PersonRegistrationBase
+    {
+        private readonly PersonOperations personOperations = new PersonOperations();
+        private readonly ILogger<PersonService> _logger;
+
+        public PersonService(ILogger<PersonService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<AddPersonResponse> AddPerson(AddPersonRequest request, ServerCallContext context)
+        {
+            var person = request.Person;
+            var people = personOperations.AddPerson(person);
+
+            _logger.Log(LogLevel.Information, "Added person: " + person.Name);
+
+            return Task.FromResult(new AddPersonResponse() { Status = AddPersonResponse.Types.Status.Success });
+        }
+
+        public override Task<Empty> PrintPeopleStream(Empty request, ServerCallContext context)
+        {
+            PersonOperations personOperations = new PersonOperations();
+            personOperations.GetPeople();
+
+            return Task.FromResult(new Empty());
+        }
+    }
+}
